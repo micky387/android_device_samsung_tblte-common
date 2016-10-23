0f02c77dd28e6f3043497c2201d78685f661388d
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2016-10-23 13:58:28 -0600

    klte-common: Add WIFI_DRIVER_NVRAM_PATH params to board config
    * Setting these helps macloader to, well you know, actually set
      calibration params for anything other than default case.
    
    Change-Id: I710a75ee58267276c702d25437042b9a0842dc8c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -156,6 +156,8 @@ WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/dhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA     := "/system/etc/wifi/bcmdhd_sta.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/system/etc/wifi/bcmdhd_apsta.bin"
 WIFI_BUS := PCIE
+WIFI_DRIVER_NVRAM_PATH_PARAM:= "/sys/module/dhd/parameters/nvram_path"
+WIFI_DRIVER_NVRAM_PATH      := "/system/etc/wifi/nvram_net.txt"
 
 # Vold
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
