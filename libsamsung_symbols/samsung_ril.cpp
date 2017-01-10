c68a998fc7e69cc185853ff3b5d81df09f8b5cd8
micky387 <mickaelsaibi@free.fr>
2017-01-10 08:31:03 +0100

    samsung_ril: update for cm-14.1

--- libsamsung_symbols/samsung_ril.cpp
+++ libsamsung_symbols/samsung_ril.cpp
@@ -15,7 +15,7 @@
  */
 
 /* status_t Parcel::writeString16 */
-extern "C" int _ZN7android6Parcel13writeString16EPKDsj();
+extern "C" int _ZN7android6Parcel13writeString16EPKDsj(unsigned int);
 extern "C" int _ZN7android6Parcel13writeString16EPKtj() {
-    return _ZN7android6Parcel13writeString16EPKDsj();
+    return _ZN7android6Parcel13writeString16EPKDsj(0);
 }
