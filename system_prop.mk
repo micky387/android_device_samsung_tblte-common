dc060490f56dd75e1103498c905e71d420517ee2
micky387 <mickaelsaibi@free.fr>
2017-02-16 03:41:48 +0100

    system_prop: add adb_logmask to radio

--- system_prop.mk
+++ system_prop.mk
@@ -73,6 +73,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.netmgrd.qos.enable=true \
+    persist.data.qmi.adb_logmask=0 \
     persist.radio.add_power_save=1 \
     persist.radio.apm_sim_not_pwdn=1 \
     persist.radio.lte_vrat_report=1 \
