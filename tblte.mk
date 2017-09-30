22e3a658a72da75e3b7aaa4d9dae2061b4755c0d
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-30 19:34:50 -0400

    tblte-common: Build netd HAL and netutils wrapper for netmgrd
    Change-Id: I93453934a2f3e06296eb4f7a62941b9b6abca703

--- tblte.mk
+++ tblte.mk
@@ -240,6 +240,12 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/vendor/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/vendor/etc/media_codecs_google_video.xml
 
+# Netutils
+PRODUCT_PACKAGES += \
+    netutils-wrapper-1.0 \
+    android.system.net.netd@1.0 \
+    libandroid_net
+
 # NFC packages
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-impl-bcm \
