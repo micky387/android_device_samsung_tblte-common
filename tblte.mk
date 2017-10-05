2d54e3c648323dda90d0546e2317f1c9233602d3
micky387 <mickaelsaibi@free.fr>
2017-10-05 20:16:30 +0200

    Overlay: Update for android oreo -Add overlay-lineage for separate overlay aosp and lineageos. -Remove too bool  not include in oreo (for now)

--- tblte.mk
+++ tblte.mk
@@ -20,7 +20,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product-if-exists, vendor/samsung/tblte-common/tblte-common-vendor.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # System properties
 -include $(LOCAL_PATH)/system_prop.mk
