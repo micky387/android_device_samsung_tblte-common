e5e1a4f55e36c2c4b1c09d278d74a6f76c443368
micky387 <mickaelsaibi@free.fr>
2017-10-21 13:07:16 +0200

    tblte-common: add BOARD_KERNEL_IMAGE_NAME

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -82,6 +82,7 @@ BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=23 msm
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02600000 --tags_offset 0x02400000 --kernel_offset 0x00008000
+BOARD_KERNEL_IMAGE_NAME := zImage
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_CONFIG := apq8084_sec_defconfig
 TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
