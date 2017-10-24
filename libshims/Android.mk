f4965e3db7f671904084e47f2f4b126e9075646c
micky387 <mickaelsaibi@free.fr>
2017-10-24 20:28:19 +0200

    tblte-common: Move SHIM libs to /system/vendor

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -6,4 +6,5 @@ LOCAL_SHARED_LIBRARIES := liblog libcutils libbinder libutils
 LOCAL_MODULE := libshim_ril
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
