2eb264b8a3f9994b99558a6cd5a8b17b9597f756
micky387 <mickaelsaibi@free.fr>
2017-10-24 14:27:20 +0200

    tblte-comon: swith to apq8084 kernel source

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -86,7 +86,7 @@ BOARD_KERNEL_IMAGE_NAME := zImage
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_CONFIG := apq8084_sec_defconfig
 TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/tblte
+TARGET_KERNEL_SOURCE := kernel/samsung/apq8084
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
 
