eaf6a1421e8c720c0a6ea59395271e009d7dcb82
micky387 <mickaelsaibi@free.fr>
2017-11-24 04:03:58 +0100

    tblte-common: Update Bluetooth for O
    *Use Samsung Bluetooth now
    *fix cp link to auto_pair_devlist
    *hack vnd_buildcfg.h for now. There is a mistake with generating this include. #endif is not add at the end of this file.

--- tblte.mk
+++ tblte.mk
@@ -107,7 +107,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth    
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/auto_pair_devlist.conf:/system/etc/auto_pair_devlist.conf \
+	$(LOCAL_PATH)/configs/auto_pair_devlist.conf:/system/etc/bluetooth/auto_pair_devlist.conf \
 	$(LOCAL_PATH)/configs/bt_did.conf:/system/etc/bluetooth/bt_did.conf \
 	$(LOCAL_PATH)/configs/bt_stack.conf:/system/etc/bluetooth/bt_stack.conf \
 	$(LOCAL_PATH)/configs/iop_bt.db:/system/etc/bluetooth/iop_bt.db \
