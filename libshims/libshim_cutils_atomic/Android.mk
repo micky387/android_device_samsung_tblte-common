087a6b4f67cb706128bd186b58eb473d4f563ed7
micky387 <mickaelsaibi@free.fr>
2017-11-28 02:56:55 +0100

    tblte-common: dont create libshim_cutils_atomic to vendor
    * libutils is in /system, not /vendor

--- libshims/libshim_cutils_atomic/Android.mk
+++ libshims/libshim_cutils_atomic/Android.mk
@@ -21,7 +21,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libshim_cutils_atomic
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_TAGS := optional
-LOCAL_PROPRIETARY_MODULE := true
 
 LOCAL_SRC_FILES := \
     atomic.c
