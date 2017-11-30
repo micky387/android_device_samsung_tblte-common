a96507f4c57b1437bfc45c01407cfac893688a5e
micky387 <mickaelsaibi@free.fr>
2017-11-30 07:29:29 +0100

    tblte-common: Enable Zcache + Zcache LZ4 compressor

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -80,7 +80,7 @@ TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x3b7 dwc3_msm.cpu_to_affin=1 androidboot.bootdevice=msm_sdcc.1
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x3b7 dwc3_msm.cpu_to_affin=1 androidboot.bootdevice=msm_sdcc.1 zcache.enabled=1 zcache.compressor=lz4
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DT := true
