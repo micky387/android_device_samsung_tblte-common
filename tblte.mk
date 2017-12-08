9ee762fb015159e835bd2218345a443d2558707d
micky387 <mickaelsaibi@free.fr>
2017-12-08 20:21:15 +0100

    tblte-common: fix memtrack module

--- tblte.mk
+++ tblte.mk
@@ -135,7 +135,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-impl \
     copybit.apq8084 \
     gralloc.apq8084 \
     hwcomposer.apq8084 \
