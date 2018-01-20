d7e1d56abb6795bfaaf4261cf65f8c59cb6d303b
micky387 <mickaelsaibi@free.fr>
2018-01-20 08:42:23 +0100

    Revert "display: Disable EGL buffer_age extension support"
    This reverts commit b5fa13ad8b98b01c0eda5f660c65c70079e2546a.
    
    Not needed: working great without this

--- system_prop.mk
+++ system_prop.mk
@@ -40,7 +40,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.egl.hw=1 \
     debug.sf.hw=1 \
     debug.gralloc.enable_fb_ubwc=1 \
-    debug.hwui.use_buffer_age=false \
     persist.hwc.enable_vds \
     sdm.perf_hint_window=50 \
     ro.opengles.version=196609 \
